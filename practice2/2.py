bb122aac7f7bf1918fd0e3eb74ba74bc10ecd696
Daria Volovyk <dvolovyk@kse.org.ua>
2025-05-14 14:30:35 +0300

    Додано до практики 2

--- practice2/2.py
+++
@@ -1,8 +0,0 @@
-a=5
-b=7
-c=0
-c=b
-b=a
-a=c
-print("a=" + str(a))
-print("b=" +str(b) )
--- practice2/5.py
+++
@@ -1,6 +0,0 @@
-from os import name
-name="молоко"
-price=30
-a=f"{name} дорівнює {price} гривень"
-print(a)
-#"f-string". f"{a} {b}, вік: {age}"
